aa819d80f997ea0ab7801bf1e70bcd301832e928
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2024-03-17 12:49:22 -0500

    Moved if to reduce indentation

--- deployed_service_update.py
+++ deployed_service_update.py
@@ -34,11 +34,13 @@ def main():
     for kustomize_file in kustomize_files:
         file_stream = io.BytesIO(kustomize_file.decoded_content)
         parsed_file = yaml.safe_load(file_stream)
-        updated_file = check_for_helm_chart_update(parsed_file)
-        if updated_file != None:
-            updated_file["path"] = kustomize_file.path
-            updated_file["sha"] = kustomize_file.sha
-            files_needing_updates.append(updated_file)
+        # Check that the file contains a helm chart
+        if "helmCharts" in kustomize_file:
+            updated_file = check_for_helm_chart_update(parsed_file)
+            if updated_file != None:
+                updated_file["path"] = kustomize_file.path
+                updated_file["sha"] = kustomize_file.sha
+                files_needing_updates.append(updated_file)
 
     # date_string = datetime.now().strftime("%Y-%m-%d")
     # main_branch = argo_repo.get_git_ref(f"heads/{argo_repo.default_branch}")
@@ -59,51 +61,49 @@ def main():
 
 
 def check_for_helm_chart_update(kustomize_file: dict):
-    # Check that the file contains a helm chart
-    if "helmCharts" in kustomize_file:
-        deployed_chart = kustomize_file["helmCharts"][0]
-        chart_repo = deployed_chart["repo"]
-        if not chart_repo.endswith("/"):
-            chart_repo += "/"
+    deployed_chart = kustomize_file["helmCharts"][0]
+    chart_repo = deployed_chart["repo"]
+    if not chart_repo.endswith("/"):
+        chart_repo += "/"
 
-        # Pull the index file containing all the available charts at the repo
-        response = requests.get(f"{chart_repo}index.yaml")
-        if not response.ok:
-            print(response.content)
-            return
-        repository_index = yaml.safe_load(io.BytesIO(response.content))
+    # Pull the index file containing all the available charts at the repo
+    response = requests.get(f"{chart_repo}index.yaml")
+    if not response.ok:
+        print(response.content)
+        return
+    repository_index = yaml.safe_load(io.BytesIO(response.content))
 
-        # Select the available chart versions from the repo
-        remote_versions = [
-            chart["version"]
-            for chart in repository_index["entries"][deployed_chart["name"]]
-        ]
+    # Select the available chart versions from the repo
+    remote_versions = [
+        chart["version"]
+        for chart in repository_index["entries"][deployed_chart["name"]]
+    ]
 
-        # Filter out prerelease versions
-        remote_versions = list(
-            filter(
-                lambda x: "dev" not in x and "alpha" not in x and "beta" not in x,
-                remote_versions,
-            )
+    # Filter out prerelease versions
+    remote_versions = list(
+        filter(
+            lambda x: "dev" not in x and "alpha" not in x and "beta" not in x,
+            remote_versions,
         )
+    )
 
-        # Sort the versions descending so the highest version is at the beginning
-        remote_versions = list(natsorted(remote_versions, reverse=True))
+    # Sort the versions descending so the highest version is at the beginning
+    remote_versions = list(natsorted(remote_versions, reverse=True))
 
-        if remote_versions[0] != deployed_chart["version"]:
-            # Return an object containing the file object with the updated version, the old, version, and the new version
-            original_version = deployed_chart["version"]
-            kustomize_file["helmCharts"][0]["version"] = remote_versions[0]
-            # return {
-            #     "kustomize_file": kustomize_file,
-            #     "original_version": original_version,
-            #     "new_version": remote_versions[0],
-            # }
+    if remote_versions[0] != deployed_chart["version"]:
+        # Return an object containing the file object with the updated version, the old, version, and the new version
+        original_version = deployed_chart["version"]
+        kustomize_file["helmCharts"][0]["version"] = remote_versions[0]
+        # return {
+        #     "kustomize_file": kustomize_file,
+        #     "original_version": original_version,
+        #     "new_version": remote_versions[0],
+        # }
 
-            # Send a discord message alerting of the new version
-            send_discord_notification(
-                f"{deployed_chart['namespace']}.{deployed_chart['releaseName']} has a new version: {remote_versions[0]}"
-            )
+        # Send a discord message alerting of the new version
+        send_discord_notification(
+            f"{deployed_chart['namespace']}.{deployed_chart['releaseName']} has a new version: {remote_versions[0]}"
+        )
 
 
 def find_kustomize_file(
