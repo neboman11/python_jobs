8f9612f0aadfebc6d763c380aa152a3195a31f2f
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2024-12-20 21:20:20 -0600

    Added better error handling for sending discord messages

--- jobs_common.py
+++ jobs_common.py
@@ -3,11 +3,14 @@ import requests
 
 
 def send_discord_notification(message: str):
-    url = f"{os.getenv("PONYBOY_BASE_URL")}/send_discord_message"
-    request = {
-        "user_id": int(os.getenv("NOTIFY_DISCORD_USER")),
-        "message": message,
-    }
-    response = requests.post(url, json=request)
-    if response.status_code > 299:
-        print(response.text)
+    try:
+        url = f"{os.getenv("PONYBOY_BASE_URL")}/send_discord_message"
+        request = {
+            "user_id": int(os.getenv("NOTIFY_DISCORD_USER")),
+            "message": message,
+        }
+        response = requests.post(url, json=request)
+        if not response.ok:
+            print(f"Failure sending Discord message: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Failure sending Discord message: {e}")
