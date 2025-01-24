378911dbbda140a3dd138b614bb4c8e8bce24a8b
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2025-01-23 20:05:35 -0600

    Added ability to update deployment images directly

--- deployed_service_update.py
+++ deployed_service_update.py
@@ -29,16 +29,20 @@ def main():
     argo_repo = gh.get_repo("neboman11/argocd-definitions")
     repo_contents = argo_repo.get_contents("/")
 
-    print("Finding kustomize files in repo")
+    print("Finding kustomize and deployment files in repo")
     kustomize_files: list[ContentFile.ContentFile] = []
-    find_kustomize_file(argo_repo, repo_contents, kustomize_files)
+    deployment_files: list[ContentFile.ContentFile] = []
+    find_kustomize_and_deployment_files(argo_repo, repo_contents, kustomize_files, deployment_files)
 
     print("Checking helm chart versions for update")
     files_needing_updates = kustomize_files_find_helm_charts_with_updates(
         kustomize_files
     )
-    
-    if len(files_needing_updates) > 0:
+
+    print("Checking image tags for update")
+    image_files_needing_updates = deployment_files_find_image_updates(deployment_files)
+
+    if len(files_needing_updates) > 0 or len(image_files_needing_updates) > 0:
         date_string = datetime.now().strftime("%Y-%m-%d")
         new_branch_name = f"service_update/{date_string}"
         target_branch = create_branch_for_chart_updates(argo_repo, new_branch_name)
@@ -48,6 +52,7 @@ def main():
         )
 
         if len(charts_to_directly_update) > 0:
+            commit_updates_to_branch(argo_repo, target_branch.ref, charts_to_directly_update)
             non_major_pull_request = create_pull_request_for_updates(
                 argo_repo, new_branch_name, target_branch.ref, charts_to_directly_update
             )
@@ -64,23 +69,33 @@ def main():
         )
 
         if len(charts_with_major_version) > 0:
+            commit_updates_to_branch(argo_repo, target_branch.ref, charts_with_major_version)
             non_major_pull_request = create_pull_request_for_updates(
                 argo_repo, new_branch_name, target_branch.ref, charts_with_major_version
             )
 
             jobs_common.send_discord_notification("Created PR for major version bumps on " + ", ".join([chart["release_name"] for chart in charts_with_major_version]))
+
+        if len(image_files_needing_updates) > 0:
+            commit_image_updates_to_branch(argo_repo, target_branch.ref, image_files_needing_updates)
+            image_pull_request = create_pull_request_for_updates(
+                argo_repo, new_branch_name, target_branch.ref, image_files_needing_updates
+            )
+
+            jobs_common.send_discord_notification("Updated image tags for " + ", ".join([image["image_name"] for image in image_files_needing_updates]))
+
     else:
-        print("Found no charts to update")
+        print("Found no charts or images to update")
 
 
 def create_pull_request_for_updates(
-    argo_repo, new_branch_name: str, target_branch_ref: str, charts_to_update
+    argo_repo, new_branch_name: str, target_branch_ref: str, files_to_update
 ):
-    commit_updates_to_branch(argo_repo, target_branch_ref, charts_to_update)
+    commit_updates_to_branch(argo_repo, target_branch_ref, files_to_update)
     pull_request = argo_repo.create_pull(
         argo_repo.default_branch,
         new_branch_name,
-        title="Automatic Helm chart version bump",
+        title="Automatic Helm chart version and image tag bump",
         body="",
     )
     return pull_request
@@ -117,6 +132,25 @@ def commit_updates_to_branch(
         argo_repo.update_file(
             file["path"],
             f"Bump {file['release_name']} version to {file['new_version']}",
+            file_content_stream.getvalue(),
+            file["sha"],
+            target_branch_ref,
+        )
+
+
+def commit_image_updates_to_branch(
+    argo_repo: Repository.Repository,
+    target_branch_ref: str,
+    files_needing_updates: list[dict[str, Any]],
+):
+    print("Committing changes to update image tags")
+    for file in files_needing_updates:
+        file_content_stream = io.StringIO()
+        yaml.dump(file["deployment_file"], file_content_stream)
+        file_content_stream.seek(0)
+        argo_repo.update_file(
+            file["path"],
+            f"Bump {file['image_name']} image tag to {file['new_tag']}",
             file_content_stream.getvalue(),
             file["sha"],
             target_branch_ref,
@@ -185,17 +219,75 @@ def check_for_helm_chart_update(kustomize_file: dict):
         }
 
 
-def find_kustomize_file(
+def find_kustomize_and_deployment_files(
     argo_repo: Repository.Repository,
     repo_files: list[ContentFile.ContentFile],
     kustomize_file_list: list[ContentFile.ContentFile],
+    deployment_file_list: list[ContentFile.ContentFile],
 ):
     for file in repo_files:
         if file.type == "file" and file.name == "kustomization.yaml":
             kustomize_file_list.append(file)
+        if file.type == "file" and file.name == "deployment.yaml":
+            deployment_file_list.append(file)
         if file.type == "dir" and file.name != "overlays":
             folder_contents = argo_repo.get_contents(f"/{file.path}")
-            find_kustomize_file(argo_repo, folder_contents, kustomize_file_list)
+            find_kustomize_and_deployment_files(argo_repo, folder_contents, kustomize_file_list, deployment_file_list)
+
+
+def deployment_files_find_image_updates(
+    deployment_files: list[ContentFile.ContentFile],
+):
+    files_needing_updates: list[dict[str, Any]] = []
+    for deployment_file in deployment_files:
+        file_stream = io.BytesIO(deployment_file.decoded_content)
+        parsed_file = yaml.safe_load(file_stream)
+        updated_file = check_for_image_update(parsed_file)
+        if updated_file != None:
+            updated_file["path"] = deployment_file.path
+            updated_file["sha"] = deployment_file.sha
+            files_needing_updates.append(updated_file)
+    return files_needing_updates
+
+
+def check_for_image_update(deployment_file: dict):
+    containers = deployment_file["spec"]["template"]["spec"]["containers"]
+    for container in containers:
+        image_name, current_tag = parse_image(container["image"])
+        new_tag = get_latest_image_tag(image_name)
+        if new_tag != current_tag:
+            container["image"] = f"{image_name}:{new_tag}"
+            return {
+                "deployment_file": deployment_file,
+                "image_name": image_name,
+                "current_tag": current_tag,
+                "new_tag": new_tag,
+            }
+
+
+def parse_image(image: str):
+    if "/" not in image:
+        image = f"docker.io/{image}"
+    image_name, tag = image.rsplit(":", 1)
+    return image_name, tag
+
+
+def get_latest_image_tag(image_name: str):
+    if image_name.startswith("docker.io/"):
+        image_name = image_name[len("docker.io/"):]
+    parts = image_name.split("/")
+    if len(parts) == 1:
+        image_name = f"library/{parts[0]}"
+    else:
+        image_name = "/".join(parts)
+
+    response = requests.get(f"https://hub.docker.com/v2/repositories/{image_name}/tags/")
+    if not response.ok:
+        print(response.content)
+        return None
+    tags = response.json()["results"]
+    latest_tag = tags[0]["name"]
+    return latest_tag
 
 
 def send_discord_notification(message):
