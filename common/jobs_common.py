d16d2bcf1d1ac8b8077ae3a1118a806a7009f33b
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2025-08-31 16:22:42 -0500

    Added retry for notification send

--- common/jobs_common.py
+++ common/jobs_common.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from requests.adapters import HTTPAdapter, Retry
 
 
 def send_notification(message: str):
@@ -15,10 +16,30 @@ def send_notification(message: str):
         url = f"{base_url}/{topic}"
         headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "text/plain"}
 
-        response = requests.post(url, headers=headers, data=message)
+        # Configure retries
+        retries = Retry(
+            total=3,  # Total number of retries
+            backoff_factor=2,  # Exponential backoff factor (2, 4, 8…)
+            status_forcelist=[
+                500,
+                502,
+                503,
+                504,
+                522,
+            ],  # Retry on these HTTP status codes
+            allowed_methods=["POST"],  # Retry only POST requests
+        )
+
+        session = requests.Session()
+        adapter = HTTPAdapter(max_retries=retries)
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+
+        response = session.post(url, headers=headers, data=message)
         if not response.ok:
             print(
-                f"Failure sending ntfy message: {response.status_code} - {response.text}"
+                f"Failed sending ntfy message: {response.status_code} - {response.text}"
             )
+
     except Exception as e:
         print(f"Failure sending ntfy message: {e}")
